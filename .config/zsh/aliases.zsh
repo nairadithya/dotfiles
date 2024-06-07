b0ac5f555a27b791117ba27053c0c1ac426528f4
Adithya Nair <adithyanair121@gmail.com>
2024-06-07 13:11:29 -0400

    Changed stuff about aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,2 +1,17 @@
 alias ls='ls --color'
 alias nv='nvim .'
+
+# >>> juliaup initialize >>>
+
+# !! Contents within this block are managed by juliaup !!
+
+case ":$PATH:" in
+    *:/home/adithya/.juliaup/bin:*)
+        ;;
+
+    *)
+        export PATH=/home/adithya/.juliaup/bin${PATH:+:${PATH}}
+        ;;
+esac
+
+# <<< juliaup initialize <<<
