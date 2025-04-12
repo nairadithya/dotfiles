f74e129b36ea2e16d0896f34f9b6b9d2b175ed0f
Adithya Nair <adithyanair121@gmail.com>
2025-04-12 16:50:18 +0530

    Added nushell and sv support

---
+++ .config/doom/modules/lang/nu/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/nu/packages.el
+
+(package! nushell-mode :recipe (:host github :repo "mrkkrp/nushell-mode"))
---
+++ .config/doom/modules/lang/sveltekit/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/sveltekit/packages.el
+
+(package! svelte-ts-mode)
