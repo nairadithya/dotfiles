29fb0de57eebe8a94648180ec98678f00df2c4d0
Adithya Nair <adithyanair121@gmail.com>
2025-09-01 23:35:06 +0530

    Moving to IosevkaTerm Slab

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -17,8 +17,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'regular))
-(setq doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'regular))
+(setq doom-font (font-spec :family "IosevkaTermSlab Nerd Font" :size 16 :weight 'regular))
+(setq doom-variable-pitch-font (font-spec :family "IosevkaTermSlab Nerd Font" :size 20 :weight 'regular))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
---
+++ .config/fuzzel/scripts/power-menu.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/lua
+local options = {
+    [" Lock"] = "hyprlock",
+    [" Shut down"] = "systemctl poweroff",
+    [" Reboot"] = "systemctl reboot",
+    ["󰍃 Log out"] = "hyprctl dispatch exit 1",
+    ["󰒲 Suspend"] = "systemctl suspend",
+    [" Hibernate"] = "systemctl hibernate",
+}
+
+local options_string = ""
+local length = 0
+for key, _ in pairs(options) do
+    options_string = options_string .. key .. "\n"
+    length = length + 1
+end
+options_string = options_string:sub(1, -2)
+
+local f = assert(
+    io.popen(
+        "echo -e '"
+            .. options_string
+            .. "' | fuzzel --dmenu "
+        ------------------ to change the location of the dropdown, use --location 1 (top left) or --location 3 (top right) (see --location in wofi --help)
+            .. length,
+        "r"
+    )
+)
+local s = assert(f:read("*a"))
+s = string.gsub(s, "^%s+", "")
+s = string.gsub(s, "%s+$", "")
+s = string.gsub(s, "[\n]+", " ")
+f:close()
+
+os.execute(options[s])
