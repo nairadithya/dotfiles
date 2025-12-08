6cb40643e6cfc30f7f2d380c883e8e954a232467
Adithya Nair <adithyanair121@gmail.com>
2025-12-08 11:32:26 +0530

    spacing and vterm setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,14 @@ alias xo='xdg-open'
 alias nvim='emacsclient -t'
 alias vi='emacsclient -t'
 alias ls='ls --color'
+
+# Emacs vterm config
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
